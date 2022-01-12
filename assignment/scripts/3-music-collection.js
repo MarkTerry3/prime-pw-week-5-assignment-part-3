c685ef8a9c49f0e8933af498fb9c47277e27909b
Mark Terry <mark.terry93@gmail.com>
2022-01-11 19:31:38 -0600

    function, and object created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  let info = {
+    title:,
+    artist:,
+    yearPublished:
+  }//end info
+}// end addToCollection
