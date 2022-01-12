cfd35e9797156d027b46b10ed7ce7e127e2f73ac
Mark Terry <mark.terry93@gmail.com>
2022-01-11 19:42:51 -0600

    dotpush

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,8 +5,9 @@ let collection = [];
 
 function addToCollection(title, artist, yearPublished){
   let info = {
-    title:,
-    artist:,
-    yearPublished:
+    title:'Let it be',
+    artist:'The Beatles',
+    yearPublished:'1970'
   }//end info
+  collection.push(info.title, info.artist, info.yearPublished)
 }// end addToCollection
