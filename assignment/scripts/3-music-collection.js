cc355305f6f2fd314246efef49abdddfbe149f91
Mark Terry <mark.terry93@gmail.com>
2022-01-12 21:25:05 -0600

    findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,6 +12,18 @@ function addToCollection(title, artist, yearPublished){
   collection.push(info)
 }// end addToCollection
 
-addToCollection('when im gone', 'eminem', '2007')
+addToCollection('Let it be', 'The Beatles', '1970')
 
 console.log(collection);
+
+let results = [];
+
+function findByArtist(artist){
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] === artist) {
+      console.log('Weve found a match', artist);
+    }// end if statement
+  }// end for loop
+}// end findByArtist
+
+findByArtist('The Beatles');
