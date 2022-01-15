103f497b015ca6e2168d4433c54c42156dfa5180
Mark Terry <mark.terry93@gmail.com>
2022-01-15 14:43:41 -0600

    showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,15 +13,27 @@ function addToCollection(title, artist, yearPublished){
 }// end addToCollection
 
 addToCollection('Let it be', 'The Beatles', '1970')
+addToCollection('Jack and Diane', 'John Mellancamp', '1982')
+
 
 console.log(collection);
+
+function showCollection(array){
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }//end for loop
+}// end showCollection
+
+showCollection(collection);
+
 
 let results = [];
 
 function findByArtist(artist){
   for (let i = 0; i < collection.length; i++) {
     if (collection[i] == artist) {
-      console.log('Weve found a match', artist);
+      console.log('Match:', artist);
     }// end if statement
   }// end for loop
 }// end findByArtist
