5737917020195f890ea6a56df75e7f3bbd4bdfed
Mark Terry <mark.terry93@gmail.com>
2022-01-16 15:23:30 -0600

    show collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,12 +4,12 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished){
-  let info = {
+  let input = {
     title: title,
     artist: artist,
     yearPublished: yearPublished
   }//end info
-  collection.push(info)
+  collection.push(input)
 }// end addToCollection
 
 addToCollection('Let it be', 'The Beatles', '1970')
@@ -18,11 +18,14 @@ addToCollection('Jack and Diane', 'John Mellancamp', '1982')
 
 console.log(collection);
 
+
+let showingCollection = [showCollection()];
+
 function showCollection(array){
   console.log(array.length);
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-  }//end for loop
+  for (let i of array) {
+    console.log(`${i.title} by ${i.artist} published in ${i.yearPublished}`);
+  }// end for loop
 }// end showCollection
 
 showCollection(collection);
@@ -33,6 +36,7 @@ let results = [];
 function findByArtist(artist){
   for (let i = 0; i < collection.length; i++) {
     if (collection[i] == artist) {
+      results.push(artist)
       console.log('Match:', artist);
     }// end if statement
   }// end for loop
